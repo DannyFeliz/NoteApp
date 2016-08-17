adb4efad10212b5a5c5320cb87832f273f4f3dec
Danny <dannyfeliz08@gmail.com>
2016-08-17 02:30:33 -0400

    Refactor resources interceptors

--- config/services.js
+++ config/services.js
@@ -3,31 +3,32 @@ import VueResource from 'vue-resource'
 import ENV from "./env.js";
 Vue.use(VueResource);
 
-if (!Object.keys(ENV).length) throw new Error ("ENV is empty");
+if (!Object.keys(ENV).length) throw new Error("ENV is empty");
 
 // Resource
-if (!ENV.API_URL) throw new Error ("API_URL is missing");
+if (!ENV.API_URL) throw new Error("API_URL is missing");
 
 Vue.http.options.root = ENV.API_URL;
 
-if (ENV.LOG_REQUEST) {
-  Vue.http.interceptors.push({
-    request: function (request) {
-      console.info("REQUEST:", request);
-      return request;
-    }
-  })
-}
+/**
+ * Log request / response
+*/
 
-if (ENV.LOG_RESPONSE) {
-  Vue.http.interceptors.push({
-    response: function (response) {
-      if (response.status > 299) {
-        console.error("RESPONSE:", response);
-      } else {
-        console.info("RESPONSE:", response);
-      }
-      return response;
+if (ENV.LOG_REQUEST || ENV.LOG_RESPONSE) {
+    Vue.http.interceptors.push((request, next) => {
+
+    if (ENV.LOG_REQUEST) {
+      console.info("REQUEST:", request);
     }
-  })
+
+    if (ENV.LOG_RESPONSE) {
+      next((response) => {
+        if (response.status > 299) {
+          console.error("RESPONSE:", response);
+        } else {
+          console.info("RESPONSE:", response);
+        }
+      });
+    }
+  });
 }
