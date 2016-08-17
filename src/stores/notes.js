62a65478305ca4d3e387a0f771fab870fb087fe0
Danny <dannyfeliz08@gmail.com>
2016-08-17 02:34:59 -0400

    Add notes store

--- src/stores/notes.js
+++ src/stores/notes.js
@@ -1,12 +1,19 @@
 import Vue from '../main';
 const store = {};
 
-store.createNote = () => {
-  new Promise((resolve, reject) => {
-
-  })
+store.createNote = (note) => {
+  return new Promise((resolve, reject) => {
+    Vue.http({
+      url: "notes",
+      method: "post",
+      body: note,
+    }).then(response => {
+      resolve(response.data)
+    }).catch(response => {
+      reject(response);
+    });
+  });
 };
-
 
 
 export default store
